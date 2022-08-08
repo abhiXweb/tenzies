efcab70a3bca53b8b8764b0e2679d3ee02404424
abhiXweb <dev.abhisharma@gmail.com>
2022-08-08 23:14:30 +0530

    added state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { useState, useEffect } from "react";
+import Die from "./components/Die";
 function App() {
+  const [dice, setDice] = useState(allNewDice());
+
+  function allNewDice() {
+    let newArr = [];
+    for (let i = 0; i < 10; i++) {
+      newArr.push(Math.ceil(Math.random() * 6));
+    }
+    return newArr
+  }
+
+  function rollDice() {
+    setDice(allNewDice())
+    console.log(dice);
+  }
+
+  const diceElement = dice.map(value => <Die value={value} />)
+
+
   return (
     <>
       <main>
-        <div className="main-in"></div>
+        <div className="dice-container">
+          {diceElement}
+        </div>
+        <button className="roll-dice-btn" onClick={rollDice}>Roll</button>
       </main>
     </>
   );
---
+++ src/components/Die.jsx
@@ -0,0 +1,8 @@
+const Die = (props) => {
+    return (
+        <div className="die-face">
+            <h2 className="die-num">{props.value}</h2>
+        </div>
+    )
+}
+export default Die
