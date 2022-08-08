42fc53f2f4dc3d0093dcd6d554364bdbbba25606
abhiXweb <dev.abhisharma@gmail.com>
2022-08-08 23:21:12 +0530

    updated readme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Die from "./components/Die";
 function App() {
   const [dice, setDice] = useState(allNewDice());
